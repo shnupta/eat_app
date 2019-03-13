f6cbd91ea10841c433f97ef1a2b520b7def9cd2b
Casey Williams <williamshoops96@gmail.com>
2019-03-13 20:53:00 +0000

    absolute mess of upgrading then downgrading from AndroidX

--- lib/pages/map_view/map_view_page.dart
+++ lib/pages/map_view/map_view_page.dart
@@ -8,13 +8,12 @@ import 'package:snacc/pages/restaurant_profile.dart';
 
 import 'package:location/location.dart';
 
-/// The [MapViewPage] displays a google map that the user interacts with to see the locations
+/// The [MapViewPage] displays a google map that the user interacts with to see the locations 
 /// of restaurants.
 class MapViewPage extends StatefulWidget {
   /// If only one restaurant is to be viewed then it will be passed in the constructor
   /// and will be the only marker on the map.
   final bool viewRestaurant;
-
   /// The restaurant to view if it is the only one.
   final Restaurant viewingRestaurant;
 
@@ -32,8 +31,6 @@ class _MapViewState extends State<MapViewPage> {
   // Set when the user clicks on a marker
   Restaurant selectedRestaurant;
 
-  Map<MarkerId, Marker> markers = {};
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,14 +41,11 @@ class _MapViewState extends State<MapViewPage> {
         builder: (BuildContext context, BoxConstraints contraints) {
           return Container(
             child: GoogleMap(
-              myLocationEnabled: true,
-              compassEnabled: true,
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(53.134304, -2.8291387),
-                zoom: 5.0,
+              options: GoogleMapOptions(
+                myLocationEnabled: true,
+                compassEnabled: true,
               ),
-              markers: Set<Marker>.of(markers.values),
+              onMapCreated: _onMapCreated,
             ),
           );
         },
@@ -63,36 +57,36 @@ class _MapViewState extends State<MapViewPage> {
     List<Restaurant> rests = await Restaurant.loadAll();
     setState(() {
       mapController = controller;
-
+      
       setState(() {
         allRestaurants = rests;
       });
     });
 
+    mapController.onMarkerTapped.add((marker) => _handleMarkerTap(marker));
+
+    mapController.onInfoWindowTapped
+        .add((infoWindow) => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RestaurantProfilePage(
+                    restaurant: selectedRestaurant,
+                  ),
+            )));
+
     // If we only want to view one specific restaurant, then zoom into that at the beginning
     if (widget.viewRestaurant) {
       double lat = widget.viewingRestaurant.latLong['latitude'];
       double long = widget.viewingRestaurant.latLong['longitude'];
-
-      Marker restMarker = Marker(
-        markerId: MarkerId(widget.viewingRestaurant.id),
+      mapController.addMarker(MarkerOptions(
         position: LatLng(lat, long),
-        infoWindow: InfoWindow(
-            onTap: _handleInfoWindowTap,
-            title: widget.viewingRestaurant.name,
-            snippet: widget.viewingRestaurant.description.substring(
+        infoWindowText: InfoWindowText(
+            widget.viewingRestaurant.name,
+            widget.viewingRestaurant.description.substring(
                     0,
                     widget.viewingRestaurant.description.length - 1 < 50
                         ? widget.viewingRestaurant.description.length - 1
                         : 50) +
                 '...'),
-        onTap: () => _handleMarkerTap(MarkerId(widget.viewingRestaurant.id)),
-      );
-
-      setState(() {
-        markers[MarkerId(widget.viewingRestaurant.id)] = restMarker;
-      });
-
+      ));
       mapController.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
           bearing: 0.0,
@@ -104,14 +98,10 @@ class _MapViewState extends State<MapViewPage> {
     } else {
       // Show all markers in view
 
-      allRestaurants.forEach((rest) => markers[MarkerId(rest.id)] = (
-        Marker(
-          markerId: MarkerId(rest.id),
-          onTap: () => _handleMarkerTap(MarkerId(rest.id)),
-            infoWindow: InfoWindow(
-                onTap: _handleInfoWindowTap,
-                title: rest.name,
-                snippet: rest.description.substring(
+      allRestaurants.forEach((rest) => mapController.addMarker(MarkerOptions(
+            infoWindowText: InfoWindowText(
+                rest.name,
+                rest.description.substring(
                         0,
                         rest.description.length - 1 < 50
                             ? rest.description.length - 1
@@ -139,27 +129,17 @@ class _MapViewState extends State<MapViewPage> {
     }
   }
 
-  /// When a marker is tapped, set the selected restaurant. This enables us to choose
+  /// When a marker is tapped, set the selected restaurant. This enables us to choose 
   /// which restaurant's profile page is opened when the info window is tapped.
-  void _handleMarkerTap(MarkerId markerId) {
+  void _handleMarkerTap(Marker marker) {
     Restaurant restaurant = allRestaurants
         .where((rest) =>
-            (rest.latLong['latitude'] as double).toStringAsPrecision(8) ==
-                markers[markerId].position.latitude.toStringAsPrecision(8) &&
-            (rest.latLong['longitude'] as double).toStringAsPrecision(8) ==
-                markers[markerId].position.longitude.toStringAsPrecision(8))
+            (rest.latLong['latitude'] as double).toStringAsPrecision(8) == marker.options.position.latitude.toStringAsPrecision(8) &&
+            (rest.latLong['longitude'] as double).toStringAsPrecision(8) == marker.options.position.longitude.toStringAsPrecision(8))
         .toList()[0];
 
     setState(() {
       selectedRestaurant = restaurant;
     });
-  }
-
-  void _handleInfoWindowTap() {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (BuildContext context) => RestaurantProfilePage(
-            restaurant: selectedRestaurant,
-          ),
-    ));
   }
 }
